93310e989ad3d2f51444d08d4f71aedc6038d525
Geofed <theglassofdawn@gmail.com>
2014-03-07 18:55:22 +0000

    PlayerCollsion I

---
+++ Assets/Scripts/PlayerCollsion.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollsion : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+	void OnCollisionEnter (Collision collision) {
+		Debug.Log("Collision!");
+	}
+
+}
+
