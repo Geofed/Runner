0e2000594621dd01c6cbe1390d330eb0a92b24f1
Geofed <theglassofdawn@gmail.com>
2014-03-20 14:47:52 +0000

    GUI upgrade and published on interwebs

---
+++ Assets/Scripts/OffAtGameOver.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class OffAtGameOver : MonoBehaviour {
+	public SpriteRenderer[] isOn;
+	public int num = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGameOver () {
+		isOn [num].enabled = false;
+	}
+}
